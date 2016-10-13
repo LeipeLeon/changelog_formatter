cacc510f625db35896776ea6bb1d2435e3b792f5
Leon Berenschot <leipeleon@gmail.com>
2016-10-13 12:02:31 +0200

    Create basic specs

--- spec/changelog_formatter_spec.rb
+++ spec/changelog_formatter_spec.rb
@@ -5,7 +5,25 @@ describe ChangelogFormatter do
     expect(ChangelogFormatter::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#to_a' do
+    it "Generates an array" do
+      expect(
+        ChangelogFormatter.to_a('spec/fixtures/CHANGELOG_1').first.lines
+      ).to eq(
+        [
+          ["enh", "Use Google Analytics code from organisation on embed page."],
+          ["enh", "remove phone from embed page & change email into support@ticketpunk.com"]
+        ]
+      )
+    end
+    it "Ignores lines startign wit a dash" do
+      expect(
+        ChangelogFormatter.to_a('spec/fixtures/CHANGELOG_2').first.lines
+      ).to eq(
+        [
+          ["enh", "remove phone from embed page & change email into support@ticketpunk.com"]
+        ]
+      )
+    end
   end
 end
